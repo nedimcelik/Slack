780b7520b56f6b7fe981b348b99926e38da64c0c
nedimcelik <54439547+abidinkarpuz@users.noreply.github.com>
2020-05-13 17:24:26 +0200

    add conflict

---
+++ src/day_1/ConflictTest.java
@@ -0,0 +1,12 @@
+package day_1;
+
+public class ConflictTest {
+    public static void main(String[] args) {
+        System.out.println("test");
+        if (true){
+            // kasjdkasd
+        }else {
+            // dfdsfllldd
+        }
+    }
+}
